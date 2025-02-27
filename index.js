b9486c50f246081071def22d3f57051edd9fd052
SARAN2046 <saranrithikb@gmail.com>
2025-02-27 22:26:27 +0530

    Fixed CORS issue

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://contactms-client-eta.vercel.app/"],
+    origin: ["https://contactms-client-eta.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
   })
