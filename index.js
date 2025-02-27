be5b34ad65c5eb523f5d2d110a7e816f1447a5a2
SARAN2046 <saranrithikb@gmail.com>
2025-02-27 22:14:00 +0530

    file changes

--- index.js
+++ index.js
@@ -3,14 +3,21 @@ import dotenv from "dotenv";
 import "./config/db.js";
 import cors from "cors";
 import { Router } from "./routes/routes.js";
+import { get } from "mongoose";
 
 const app = express();
 app.use(express.json());
-app.use(cors());
-app.use("/contactms",Router)
+app.use(
+  cors({
+    origin: ["https://contactms-client-eta.vercel.app/"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+  })
+);
+app.use("/contactms", Router);
 
-dotenv.config({path:"./config/.env"})
+dotenv.config({ path: "./config/.env" });
 
-app.listen(process.env.PORT,()=>{
-  console.log("Server is running")
-})
+app.listen(process.env.PORT, () => {
+  console.log("Server is running");
+});
